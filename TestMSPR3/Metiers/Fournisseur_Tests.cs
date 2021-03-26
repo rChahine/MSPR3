a42a2a7cadb7ad2f3c03355d85be8828daefdeb8
Pierrick Loiacono <pierrick38800@gmail.com>
2021-03-26 14:40:27 +0100

    🎨 Refactor project

---
+++ TestMSPR3/Metiers/Fournisseur_Tests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSPR3.Metiers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSPR3.Metiers.Tests
+{
+    [TestClass()]
+    public class Fournisseur_Tests
+    {
+        [TestMethod()]
+        public void FournisseurTests()
+        {
+            Fournisseur fournisseur = new Fournisseur(1, "test", "test", null);
+            Assert.IsInstanceOfType(fournisseur, typeof(Fournisseur));
+        }
+
+        [TestMethod()]
+        public void FournisseurAttributesTests()
+        {
+            Fournisseur fournisseur = new Fournisseur(1, "test", "test", null);
+            var result = fournisseur.Adresse;
+            Assert.AreEqual(result, "test");
+        }
+    }
+}
---
+++ TestMSPR3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestMSPR3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestMSPR3")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2091d4e7-78f5-4afa-8c8e-efa8b98a27da")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
