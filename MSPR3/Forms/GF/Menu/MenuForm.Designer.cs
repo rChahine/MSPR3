ce224f2528fb50035685888c5637dcc296fa51dc
AmongTheDevil <AmongTheDevil@users.noreply.github.com>
2021-04-08 16:12:46 +0200

    🚧 Mise en forme de MenuForm.cs + Création de 3 form

--- MSPR3/Forms/GF/Menu/MenuForm.Designer.cs
+++ MSPR3/Forms/GF/Menu/MenuForm.Designer.cs
@@ -29,12 +29,83 @@ namespace MSPR3.Forms.GF.Menu
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.TitreMenuFinanciere = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // TitreMenuFinanciere
+            // 
+            this.TitreMenuFinanciere.AutoSize = true;
+            this.TitreMenuFinanciere.ForeColor = System.Drawing.SystemColors.Window;
+            this.TitreMenuFinanciere.Location = new System.Drawing.Point(244, 51);
+            this.TitreMenuFinanciere.Name = "TitreMenuFinanciere";
+            this.TitreMenuFinanciere.Size = new System.Drawing.Size(154, 17);
+            this.TitreMenuFinanciere.TabIndex = 3;
+            this.TitreMenuFinanciere.Text = "GESTION FINANCIERE";
+            this.TitreMenuFinanciere.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel1.LinkColor = System.Drawing.Color.White;
+            this.linkLabel1.Location = new System.Drawing.Point(52, 154);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(167, 17);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Gestion des prix d\'achats";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel2.LinkColor = System.Drawing.Color.White;
+            this.linkLabel2.Location = new System.Drawing.Point(52, 212);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(166, 17);
+            this.linkLabel2.TabIndex = 5;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Gestion des fournisseurs";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel3.LinkColor = System.Drawing.Color.White;
+            this.linkLabel3.Location = new System.Drawing.Point(52, 272);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(79, 17);
+            this.linkLabel3.TabIndex = 6;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "Facturation";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // MenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.TitreMenuFinanciere);
+            this.Name = "MenuForm";
             this.Text = "MenuForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label TitreMenuFinanciere;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel3;
     }
 }
--- MSPR3/Forms/GF/Menu/MenuForm.cs
+++ MSPR3/Forms/GF/Menu/MenuForm.cs
@@ -15,6 +15,27 @@ namespace MSPR3.Forms.GF.Menu
         public MenuForm()
         {
             InitializeComponent();
+
+            TitreMenuFinanciere.Font = new System.Drawing.Font(TitreMenuFinanciere.Font.Name, 16F);
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Forms.GF.GestionAchat.GestionAchatForm GestionA = new Forms.GF.GestionAchat.GestionAchatForm();
+            GestionA.ShowDialog();
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Forms.GF.Facturation.FacturationForm Facturation = new Forms.GF.Facturation.FacturationForm();
+            Facturation.ShowDialog();
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Forms.GF.GestionFournisseur.GestionFournisseurForm GestionF = new Forms.GF.GestionFournisseur.GestionFournisseurForm();
+            GestionF.ShowDialog();
         }
     }
 }
