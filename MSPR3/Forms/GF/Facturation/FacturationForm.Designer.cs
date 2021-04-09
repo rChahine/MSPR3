0bae33680a176583a67692283f98aa0c2bc0effb
AmongTheDevil <AmongTheDevil@users.noreply.github.com>
2021-04-09 13:10:47 +0200

    🚧 Mis en forme page facturation

--- MSPR3/Forms/GF/Facturation/FacturationForm.Designer.cs
+++ MSPR3/Forms/GF/Facturation/FacturationForm.Designer.cs
@@ -28,19 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.TitreMenuFacturation = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 104);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(776, 292);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // TitreMenuFacturation
+            // 
+            this.TitreMenuFacturation.AutoSize = true;
+            this.TitreMenuFacturation.Location = new System.Drawing.Point(255, 65);
+            this.TitreMenuFacturation.Name = "TitreMenuFacturation";
+            this.TitreMenuFacturation.Size = new System.Drawing.Size(79, 17);
+            this.TitreMenuFacturation.TabIndex = 0;
+            this.TitreMenuFacturation.Text = "Facturation";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(594, 402);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ajouter une facture";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // FacturationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.TitreMenuFacturation);
             this.Name = "FacturationForm";
             this.Text = "FacturationForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label TitreMenuFacturation;
+        private System.Windows.Forms.Button button1;
     }
 }
--- MSPR3/Forms/GF/Facturation/FacturationForm.cs
+++ MSPR3/Forms/GF/Facturation/FacturationForm.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,37 @@ namespace MSPR3.Forms.GF.Facturation
         public FacturationForm()
         {
             InitializeComponent();
+
+            TitreMenuFacturation.Font = new System.Drawing.Font(TitreMenuFacturation.Font.Name, 16F);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog OFD = new OpenFileDialog();
+            OFD.Filter = "pdf files (*.pdf)|*.pdf|All files (*.*)|*.*";
+            OFD.FilterIndex = 2;
+            OFD.RestoreDirectory = true;
+
+            if (OFD.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+                var filePath = OFD.FileName;
+                var fileStream = OFD.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    var fileContent = reader.ReadToEnd();
+                }
+
+                listBox1.Items.Add(filePath);
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var file = (FileInfo)listBox1.SelectedItem;
+            Process.Start(file.FullName); 
         }
     }
 }
