da9540de98401a02962f74dcba2af222df212734
Romain CHAHINE <romain.chahine@rwigo.com>
2021-05-05 14:26:20 +0200

    💚 update jenkins deployement script

---
+++ jenkins/deploy.groovy
@@ -0,0 +1,37 @@
+pipeline {
+    agent any
+    stages {
+        stage('generate a release exe file') {
+            steps {
+                bat '''
+                    dotnet build --configuration Release
+                '''
+            }
+        }
+        stage('Pack all files in portable directory') {
+            steps {
+                bat '''
+                    mkdir .\v1.2.3
+                    move .\MSPR3.App\bin\Release\netcoreapp3.1\* .\v1.2.3\
+                '''
+            }
+        }
+        stage('Send folder to git') {
+            steps {
+                bat '''
+                    git checkout -b "v1.2.3"
+                    git rm -r --cached .
+                    git add v1.2.3
+                    git commit -m "🔖 v1.2.3"
+                    git push origin v1.2.3 --tags
+                '''
+            }
+        }
+    }
+    post {
+        always {
+            echo 'Delete directory'
+            deleteDir()
+        }
+    }
+}
