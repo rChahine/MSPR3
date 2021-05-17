51f233aa9e28e44d36891407966d5d2e427b1480
Romain CHAHINE <romain.chahine@rwigo.com>
2021-05-17 18:29:39 +0200

    🐛 restore GA menu

--- MSPR3.App/Forms/GA/Menu/MenuForm.cs
+++ MSPR3.App/Forms/GA/Menu/MenuForm.cs
@@ -7,17 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CR = MSPR3.Forms.GA.CompteResultat;
 
 namespace MSPR3.Forms.GA.Menu
 {
     public partial class MenuForm : Form
     {
-        /// <summary>
-        /// Constructeur de la classe
-        /// </summary>
         public MenuForm()
         {
             InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            CR.CompteResultat cr = new CR.CompteResultat();
+            cr.ShowDialog();
         }
     }
 }
